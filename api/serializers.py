f2d695f8813481e36367303c7aa2f18a9317557c
Isabela-Nunes <isabelarcn@gmail.com>
2021-11-02 20:00:06 +0000

    Updated: Create, include error for repeated project name

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 
 from .models import PackageRelease, Project
-from .pypi import version_exists, latest_version, project_name
+from .pypi import version_exists, latest_version
 
 
 class PackageSerializer(serializers.ModelSerializer):
@@ -11,11 +11,6 @@ class PackageSerializer(serializers.ModelSerializer):
         extra_kwargs = {"version": {"required": False}}
 
     def validate(self, data):
-
-    #    if package_name in data:
-    #        raise serializers.ValidationError({"error": "Project name already exists"})
-    #    else:
-    #        pass
 
         found = False
         for i in data:
@@ -40,6 +35,7 @@ class PackageSerializer(serializers.ModelSerializer):
         
         return data
 
+
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
@@ -50,13 +46,19 @@ class ProjectSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         
         packages = validated_data["packages"]
-        projeto = Project.objects.create(name=validated_data["name"])
+        proj_new = Project.objects.create(name=validated_data["name"])
         
+        dic_pack = {}
         length_pack = len(packages)
         i = 0
         while i < length_pack:
-            package = PackageRelease.objects.create(name=packages[i]['name'], version=packages[i]['version'], project=projeto)
-            i += 1
+            if (packages[i]['name'], packages[i]['version']) in dic_pack.items():
+                proj_new.delete()
+                raise serializers.ValidationError({"Error:""project name already exists"})
+            else:
+                package = PackageRelease.objects.create(name=packages[i]['name'], version=packages[i]['version'], project=proj_new)
+                dic_pack[packages[i]['name']] = packages[i]['version']
+                i += 1
         
-        projeto.save()
-        return projeto
+        proj_new.save()
+        return proj_new
